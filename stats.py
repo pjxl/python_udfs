4d1fa4156bdba1ae62bec6ed47c182b194696ef2
Patrick Laughlin <pjudelaughlin@gmail.com>
2022-01-14 14:58:07 -0500

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,3 +1,10 @@
+def conf_int(x, stat, se):  
+  ci_lwr = x - stat * se
+  ci_upr = x + stat * se
+  
+  return ci_lwr, ci_upr
+
+
 def ztest_2prop(x_treat, n_treat, x_ctrl, n_ctrl, alpha=0.05, ha='two-sided'):
   """
     Conduct a two-proportion z-test.
@@ -53,8 +60,10 @@ def ztest_2prop(x_treat, n_treat, x_ctrl, n_ctrl, alpha=0.05, ha='two-sided'):
 
   # Find the lower and upper CIs
   # n.b.: in units of the difference between p_treat and p_ctrl
-  ci_lwr = p_treat - p_ctrl - z_critical*se
-  ci_upr = p_treat - p_ctrl + z_critical*se
+#   ci_lwr = p_treat - p_ctrl - z_critical*se
+#   ci_upr = p_treat - p_ctrl + z_critical*se
+
+  ci_lwr, ci_upr = conf_int(p_treat-p_ctrl, z_critical, se)
 
   # Calculate the pct lift
   lift = p_treat/p_ctrl-1
