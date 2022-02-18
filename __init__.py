c1c8a6919f2528863bd654a0e453573fea5b515e
Patrick Laughlin <pjudelaughlin@gmail.com>
2022-02-18 16:08:25 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,1 @@
-import numpy as np
-import pandas as pd
-import math
-import scipy.stats as stats
-import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
-import matplotlib.dates as mdate
-import seaborn as sns
-from google.colab import auth
-
 from . import test
