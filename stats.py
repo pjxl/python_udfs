2438c84ddbdcc3a40728fd7090a1ee0526da579d
Patrick Laughlin <pjudelaughlin@gmail.com>
2022-03-22 18:32:08 -0400

    Add F-score function

--- stats.py
+++ stats.py
@@ -199,3 +199,18 @@ def welch_ttest(treat, ctrl, alpha=0.05, ha='two-sided'):
 
     with pd.option_context('display.precision', 10):
         return out_df
+
+    
+def f_score(precision, recall, beta=1):
+    """
+    Calculate the F-score for the given `precision`, `recall`, and `beta` values.
+
+    Parameters
+    ----------
+        precision (float): The precision estimate, which must be a value in the range [0, 1].
+
+        recall (float): The recall estimate, which must be a value in the range [0, 1].
+
+        beta (int): The beta value to be used. Defaults to 1, in which case the return value is equivalent to the F1 score.
+    """
+    return (1+beta**2) * (precision*recall) / (beta**2*precision+recall)    
