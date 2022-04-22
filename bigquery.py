743c717593aaa1483accaa3ee38064cf86417fdc
Patrick Laughlin <pjudelaughlin@gmail.com>
2022-04-22 15:58:27 -0400

    Update bigquery.py

--- bigquery.py
+++ bigquery.py
@@ -1,4 +1,15 @@
+from google.cloud import bigquery
 from google.colab import auth
 
+
 def bq_auth():
-  return auth.authenticate_user()
+    return auth.authenticate_user()
+
+
+class BQClient(project='etsy-bigquery-adhoc-prod'):
+    def __init__(self, project):
+        self.client = bigquery.Client(project=project)
+        
+        
+    def query(self, sql):
+        return self.client.query(sql).to_dataframe()
