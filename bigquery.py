a7249fee65ad4891d62f7dd815b491ed9ef32919
Patrick Laughlin <pjudelaughlin@gmail.com>
2022-05-17 13:44:23 -0400

    Update bigquery.py

--- bigquery.py
+++ bigquery.py
@@ -10,10 +10,14 @@ def bq_auth():
     return auth.authenticate_user()
 
 
-class BQClient():
-    def __init__(self, project='etsy-bigquery-adhoc-prod'):
-        self.client = bigquery.Client(project=project)
+class BigQueryClient():
+    def __init__(self, project='etsy-bigquery-adhoc-prod', params=dict():
+        self.params = params
+        try:
+            self.client = bigquery.Client(project=project)
         
-        
+        try:
+            bq_auth()        
+          
     def query(self, sql):
         return self.client.query(sql).to_dataframe()
