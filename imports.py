d6f8d8501ed9df8d9d567438dfbff1077bb49058
Patrick Laughlin <pjudelaughlin@gmail.com>
2022-10-25 08:19:12 -0400

    change stats alias

--- imports.py
+++ imports.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import math
-import scipy.stats as stats
+import scipy.stats as st
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import matplotlib.dates as mdate
