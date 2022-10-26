bd4acf2a9b3cf2b0aee37f92596e217548097f21
Patrick Laughlin <pjudelaughlin@gmail.com>
2022-10-26 11:25:27 -0400

    standardize

--- stats/inference/estimation/proportion.py
+++ stats/inference/estimation/proportion.py
@@ -1,38 +1,40 @@
 import math
 import scipy.stats as st
 import numpy as np
+import numpy.typing as npt
+from typing import Union, Tuple
 
 
 def sample_size(
-	d: float, p: float=0.5, N: int=None, cl: float=0.95
-	) -> int:
+    moe: float, prop: float=0.5, popl_size: int=None, alpha: float=0.05
+    ) -> int:
     """
     Calculates the minimum sample size needed to estimate the proportion of observations within 
     a population that have a given characteristic, while meeting a given constraint on precision.
 
     Parameters
     ----------
-        d : float in (0, 1)
-            The required margin of error, or the half-length of the desired confidence interval.
-        p : float in (0, 1), default 0.5
+        moe : float in (0, 1)
+            The desired margin of error, or the half-length of the desired confidence interval.
+        prop : float in (0, 1), default 0.5
             Prior assumption around the proportion of the characteristic. Defaults to 0.5, which returns
             the most conservatively (large) sample size.
-        N : int, default None
+        popl_size : int, default None
             The size of the population. When `None`, assume an infinite population, and ignore the finite
-            population correction (fpc).
-        cl : float in (0, 1), default 0.5
-            The desired confidence level, defaulting to 0.95 or 95%.
+            population correction (FPC).
+        alpha : float in (0, 1), default 0.5
+            The desired alpha level (1 - confidence level), defaulting to 0.05, i.e a 95% CL.
 
     Returns
     -------
         n : int
-            The sample size needed to observe a population proportion as large as `p`, at confidence
-            level `cl`, with a margine of error `d`. Round decimal values up to the nearest integer
+            The sample size needed to observe a population proportion as large as `prop`, at alpha
+            level `alpha`, with a margin of error `moe`. Round decimal values up to the nearest integer
             using `math.ceil()`.
 
     Notes
     -----
-    When `N=None`, equivalent to `statsmodels.stats.proportion.samplesize_confint_proportion()`:
+    When `popl_size=None`, equivalent to `statsmodels.stats.proportion.samplesize_confint_proportion()`:
     https://www.statsmodels.org/dev/generated/statsmodels.stats.proportion.samplesize_confint_proportion.html
 
     References
@@ -41,20 +43,17 @@ def sample_size(
 
     """
 
-    # Find alpha value
-    alpha = 1-cl
-
-    # Find z-score
-    z = st.norm.ppf(1-alpha/2)
+    # Find critical value (z-score)
+    cv = st.norm.ppf(1-alpha/2)
 
     # If N is provided, assume a finite population and use the finite population correction
-    if N:
-        num = N*p*(1-p)
-        denom = (N-1)*(d**2)/(z**2) + p*(1-p)
+    if popl_size:
+        num = popl_size*prop*(1-prop)
+        denom = (popl_size-1)*(moe**2)/(cv**2) + prop*(1-prop)
     # Otherwise, assume an infinite population
     else:
-        num = z**2*p*(1-p)
-        denom = d**2
+        num = cv**2*prop*(1-prop)
+        denom = moe**2
 
     # Return sample size
     # Handle fractional sizes by returning the ceiling
@@ -62,25 +61,25 @@ def sample_size(
 
 
 def confint(
-	p: float, n: int, cl: float=0.95
-	) -> tuple:
+    prop: float, sample_size: int, alpha: float=0.05
+    ) -> Tuple[float]:
     """
     Calculates the confidence interval for an estimate of a population proportion, using
     the normal approximation.
 
     Parameters
     ----------
-        p : float in (0, 1)
+        prop : float in (0, 1)
             The observed proportion of observations in a sample having a given characteristic.
-        n : int
+        sample_size : int
             The number of observations in the sample.
-        cl : float in (0, 1), default 0.5
-            The desired confidence level, defaulting to 0.95 or 95%.
+        alpha : float in (0, 1), default 0.5
+            The desired alpha level (1 - confidence level), defaulting to 0.05, i.e a 95% CL.
 
     Returns
     -------
-        ci_low, ci_upper : tuple
-            The lower and upper bounds of the confidence interval around `p`.
+        ci_lower, ci_upper : tuple of floats
+            The lower and upper bounds of the confidence interval around `prop`.
 
     Notes
     -----
@@ -91,16 +90,109 @@ def confint(
     .. [*] https://online.stat.psu.edu/stat506/lesson/2/2.2
     """
 
-    # Find alpha value
-    alpha = 1-cl
-
-    # Find z-score
-    z = st.norm.ppf(1-alpha/2)
+    # Find critical value (z-score)
+    cv = st.norm.ppf(1-alpha/2)
 
     # Find variance in p
-    var_p = p*(1-p)/n
+    var = prop*(1-prop)/sample_size
 
     # Find margin of error
-    d = z*math.sqrt(var_p)
+    moe = cv*math.sqrt(var)
 
-    return p - d, p + d
+    # Find lower and upper bounds of CI
+    ci_lower, ci_upper = prop-moe, prop+moe
+
+    return ci_lower, ci_upper
+
+
+def strat_proportion(
+    props: Union[npt.NDArray[np.float64], 'pd.Series[np.float64]'], 
+    strat_sizes: Union[npt.NDArray[np.int64], 'pd.Series[np.int64]']
+    ) -> np.float64:
+    """
+    Calculate the weighted proportion within a stratified sample.
+
+    Parameters
+    ----------
+        props : array-like of floats
+            The proportions observed in each sample.
+        strat_sizes : array-like of ints
+            The number of observations in each stratum, whose grand total equals the size of the population.
+
+    Returns
+    -------
+        p : float
+            The weighted proportion across the entire stratified sample.
+
+    Notes
+    -----
+
+    References
+    ----------
+    .. [*] Lohr, S.: "Sampling: Design and Analysis", 2nd ed., ch. 3 (93-95)
+    .. [*] https://stattrek.com/survey-research/stratified-sampling-analysis
+    """
+    
+    # Find total population size
+    popl_size = np.sum(strat_sizes)
+    
+    # Find sampling fraction (stratum weights)
+    weights = strat_sizes/popl_size
+
+    return np.sum(weights*props)
+
+
+def strat_confint(
+    props: Union[npt.NDArray[np.float64], 'pd.Series[np.float64]'], 
+    sample_sizes: Union[npt.NDArray[np.int64], 'pd.Series[np.int64]'], 
+    strat_sizes: Union[npt.NDArray[np.int64], 'pd.Series[np.int64]'], 
+    alpha: float=0.05
+    ):
+    """
+    In a stratified sample setting, calculates the confidence interval for an estimate of a population proportion.
+
+    Parameters
+    ----------
+        props : array-like of floats
+            The proportions observed in each sample.
+        sample_sizes : array-like of ints
+            The sample size of each stratum.
+        strat_sizes : array-like of ints
+            The number of observations in each stratum, whose grand total equals the size of the population.
+
+    Returns
+    -------
+        ci_low, ci_upper : tuple
+            The lower and upper bounds of the confidence interval around the (weighted) sample proportion.
+
+    Notes
+    -----
+
+    References
+    ----------
+    .. [*] Lohr, S.: "Sampling: Design and Analysis", 2nd ed., ch. 3 (93-95)
+    .. [*] https://stattrek.com/survey-research/stratified-sampling-analysis
+    """
+
+    # Find critical value (z-score)
+    cv = st.norm.ppf(1-alpha/2)
+
+    # Find total population size
+    popl_size = np.sum(strat_sizes)
+
+    # Find weighted sample proportion
+    prop = strat_proportion(props, strat_sizes)
+    
+    # Find variance of each stratum proportion
+    var = sample_sizes/(sample_sizes-1) * props * (1-props)
+
+    # Find standard error
+    se = np.sqrt(np.sum(strat_sizes**2 * (1/popl_size)**2 * (1-sample_sizes/strat_sizes) * var/sample_sizes))
+
+    # Find margin of error
+    moe = cv*se
+
+    # Find lower and upper bounds of CI
+    ci_lower, ci_upper = prop-moe, prop+moe
+
+    return ci_lower, ci_upper
