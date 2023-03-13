1cf6ea80596557e4415bfd2c1117bcdf23cc7781
Patrick Laughlin <pjudelaughlin@gmail.com>
2023-03-13 18:36:49 -0400

    Update monte_carlo.py

--- stats/inference/simulation/monte_carlo.py
+++ stats/inference/simulation/monte_carlo.py
@@ -9,32 +9,25 @@ class MonteCarlo:
     """
     def __init__(self, seed=None):
         self.seed = seed
-        self.distribution, self.distr_args, self.distr_pdf = {}, {}, {}
-        self.var = []
+        self.variables = []
+        self.distributions, self.args, self.pdfs = {}, {}, {}
 
 
+    # Initialize a random number generator based on the provided seed
     def _initialize_rng(self):
         self.rng = np.random.default_rng(self.seed)
     
-
+    # Map a variable to its specified distribution and arguments
+    # These are stored as properties
     def _fit_var(self, var, distribution, **kwargs):
-        self.distribution.update({var: distribution})
-        self.distr_args.update({var: {**kwargs}})
-
-        if var not in self.var:
-            self.var.append(var)
+        self.distributions.update({var: distribution})
+        self.args.update({var: {**kwargs}})
+        if var not in self.variables:
+            self.variables.append(var)
     
-
-    def _draw(self, var, write=True):
-        sample = self.distribution[var](**self.distr_args[var])
-
-        if write:
-            if var in self.distr_samples:
-                self.distr_samples[var].append(sample)
-            else:
-                self.distr_samples.update({var: [sample]})        
-
-        return sample
+    # For a given variabe, draw a single random sample from its distribution
+    def _draw(self, var):
+        return self.distributions[var](**self.args[var])
 
 
     def _plot_pdf(self, x, n, fig=None, ax=None, title=None, xlab=None, confint=None):
@@ -89,42 +82,48 @@ class MonteCarlo:
         self._fit_var(var, PERT_distribution, low=low, mode=mode, high=high, lamb=lamb)
 
 
-    def plot_sampling_distribution(self, component, n=1000, fig=None, ax=None):
+    def plot_sampling_distribution(self, var, n=1000, fig=None, ax=None):
         self._initialize_rng()
 
-        if isinstance(component, str) and component in self.distribution:
+        if isinstance(var, str) and var in self.distributions:
             samples = []
             for i in range(n):
-                samples.append(self._draw(component, write=False))            
+                samples.append(self._draw(var))            
             
             self._plot_pdf(samples, n, fig=fig, ax=ax)
 
 
-    def simulate(self, func=None, n=1000, confint_alpha=0.05):
+    def simulate(self, func, n=1000):
         self._initialize_rng()
-        self.simulation = []
-        self.distr_samples = {}
+        self.simulations = []
+        self.samples = dict.fromkeys(self.distributions.keys(), [])
 
-        if func and callable(func):
+        if callable(func):
             for i in range(n):
-                samples = {}
-                for var in self.distribution.keys():
-                    samples.update({var: self._draw(var)})
+                sample_i = {}
+                for var in self.distributions.keys():
+                    val = self._draw(var)
+                    sample_i.update({var: val})
+                    self.samples[var].append(val)
                 
-                self.simulation.append(func(**samples))
+                self.simulations.append(func(**sample_i))
             
-            for var in self.distribution.keys():
-                p, _ = self._plot_pdf(self.distr_samples[var], n, title=f'Monte Carlo sampling distribution: {var}')
-                self.distr_pdf.update({var: p})
+            for var in self.distributions.keys():
+                p, _ = self._plot_pdf(self.samples[var], 
+                                      len(self.samples[var]), 
+                                      title=f'Monte Carlo sampling distribution: {var}')
+                self.pdfs.update({var: p})
                 plt.close()
 
+        self.simulations = np.array(self.simulations)
+
     
-    def simulation_confint(self, alpha):
-        pctiles = alpha/2, 1-alpha/2
-        ci_lower, ci_upper = np.quantile(self.simulation, np.array(pctiles))
-        return ci_lower, ci_upper
+    # def simulation_confint(self, alpha):
+    #     pctiles = alpha/2, 1-alpha/2
+    #     ci_lower, ci_upper = np.quantile(self.simulations, np.array(pctiles))
+    #     return ci_lower, ci_upper
 
 
-    def plot_simulation_results(self, fig=None, ax=None, confint=None):
-        if self.simulation:  
-            return self._plot_pdf(self.simulation, n=len(self.simulation), confint=confint, fig=fig, ax=ax)
+    def plot_simulation_distribution(self, fig=None, ax=None, confint=None):
+        if hasattr(self, 'simulations'):  
+            return self._plot_pdf(self.simulations, n=len(self.simulations), confint=confint, fig=fig, ax=ax)
