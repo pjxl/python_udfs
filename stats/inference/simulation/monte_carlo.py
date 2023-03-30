11700bf28ee4578337e2ed88030f4b410bb6c944
Patrick Laughlin <pjudelaughlin@gmail.com>
2023-03-30 14:41:15 -0400

    refactor to make more pythonic

--- stats/inference/simulation/monte_carlo.py
+++ stats/inference/simulation/monte_carlo.py
@@ -1,131 +1,163 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from collections import defaultdict
 
 
 class MonteCarlo:
-    """
-    TODO: Annotation
-    """
-    def __init__(self, seed=None):
+    def __init__(self, seed):
         self.seed = seed
-        self.variables = []
-        self.distributions, self.args, self.pdfs = {}, {}, {}
+        self.rng = None
+        self.inputs = {}
+        self.pdf = self.DistributionGenerator(self)
 
-
+    
     # Initialize a random number generator based on the provided seed
     def _initialize_rng(self):
         self.rng = np.random.default_rng(self.seed)
-    
-    # Map a variable to its specified distribution and arguments
-    # These are stored as properties
-    def _fit_var(self, var, distribution, **kwargs):
-        self.distributions.update({var: distribution})
-        self.args.update({var: {**kwargs}})
-        if var not in self.variables:
-            self.variables.append(var)
-    
-    # For a given variabe, draw a single random sample from its distribution
-    def _draw(self, var):
-        return self.distributions[var](**self.args[var])
 
 
-    def _plot_pdf(self, x, n, fig=None, ax=None, title=None, xlab=None, confint=None):
-        if not fig and not ax:
-            fig, ax = plt.subplots()
+    def _check(self, names):
+        names = set(names)
+        initialized_names = set(self.inputs.keys())
         
-        sns.histplot(x, ax=ax, kde=True, stat='probability')
-        ax.set_xlabel('# resamples: {:,.0f}'.format(n))
-        ax.set_title(title)
-        
-        if confint:
-            ax.vlines(confint, *ax.get_ylim(), linestyle='dashed', colors=['black']*2)
-        
-        return fig, ax
+        if names.issubset(initialized_names):
+            pass
+        else:
+            uninit_inputs = names.difference(initialized_names)
+            msg = f"Parameter named '{list(uninit_inputs)[0]}' has not been defined as an input in {self.__class__.__name__} object."
+            msg += f' Defined inputs are: {list(self.inputs.keys())}.'
+            raise NameError(msg)
 
 
-    def exact_value(self, var, val):
-        def degenerate_distribution(**kwargs):
-            return self.rng.uniform(low=val, high=val)
-
-        self._fit_var(var, degenerate_distribution, val=val)
-
-
-    def uniform(self, var, low, high):
-        def uniform_distribution(**kwargs):
-            return self.rng.uniform(low, high)
-        
-        self._fit_var(var, uniform_distribution, low=low, high=high)
-
-
-    def normal(self, var, center, stddev):
-        def normal_distribution(**kwargs):
-            return self.rng.normal(loc=center, scale=stddev)
-        
-        self._fit_var(var, normal_distribution, center=center, stddev=stddev)
-
-
-    def triangle(self, var, low, mode, high):
-        def triangle_distribution(**kwargs):
-            return self.rng.triangular(left=low, mode=mode, right=high)
-        
-        self._fit_var(var, triangle_distribution, low=low, mode=mode, high=high)
-
-
-    def PERT(self, var, low, mode, high, lamb=4):
-        def PERT_distribution(**kwargs):
-            r = high - low
-            alpha = 1 + lamb * (mode - low) / r
-            beta = 1 + lamb * (high - mode) / r        
-            return low + self.rng.beta(alpha, beta) * r
-        
-        self._fit_var(var, PERT_distribution, low=low, mode=mode, high=high, lamb=lamb)
-
-
-    def plot_sampling_distribution(self, var, n=1000, fig=None, ax=None):
+    def simulate(self, func, n=10000):
         self._initialize_rng()
-
-        if isinstance(var, str) and var in self.distributions:
-            samples = []
-            for i in range(n):
-                samples.append(self._draw(var))            
-            
-            self._plot_pdf(samples, n, fig=fig, ax=ax)
-
-
-    def simulate(self, func, n=1000):
-        self._initialize_rng()
-        self.simulations = []
-        self.samples = defaultdict(list)
+        simulations = []
+        samples = defaultdict(list)
 
         if callable(func):
-            for i in range(n):
-                sample_i = {}
-                for var in self.distributions.keys():
-                    val = self._draw(var)
-                    sample_i.update({var: val})
-                    self.samples[var].append(val)
-                
-                self.simulations.append(func(**sample_i))
-            
-            for var in self.distributions.keys():
-                p, _ = self._plot_pdf(self.samples[var], 
-                                      len(self.samples[var]), 
-                                      title=f'Monte Carlo sampling distribution: {var}')
-                self.pdfs.update({var: p})
-                plt.close()
+            func_args = set(func.__code__.co_varnames)
+        else:
+            raise TypeError('Argument passed to `func` must be a callable.')
 
-        self.simulations = np.array(self.simulations)
-        self.samples = dict(self.samples)
+        self._check(func_args)
+
+        for i in range(n):
+            sample_i = {}
+            for name in func_args:
+                val = self.inputs[name].draw()
+                sample_i.update({name: val})
+                samples[name].append(val)
+            
+            simulations.append(func(**sample_i))
+
+        return self.SimulationArray(a=simulations, samples=pd.DataFrame(samples))
+
+
+    # An InputDict is a dict corresponding to a single user-defined input to the MonteCarlo model
+    # An InputDict is typically created through method calls made to the MonteCarlo object's `pdf` attribute
+    class InputDict(dict):
+        def __init__(self, outer, obj):
+            self.outer = outer
+            return super().__init__(obj)
+
+
+        # Draw a single random sample from the input's distribution
+        def draw(self):
+            if not self.outer.rng:
+                self.outer._initialize_rng()
+
+            f = self.get('function')
+            kwargs = self.get('args')
+            return f(**kwargs)
+
+
+        # Plot the sampling distribution of the input
+        def plot(self, n=10000, fig=None, ax=None):
+            self.outer._initialize_rng()
+            
+            samples = []
+            for i in range(n):
+                samples.append(self.draw())            
+            
+            if not fig and not ax:
+                fig, ax = plt.subplots()
+
+            sns.histplot(samples, ax=ax, kde=True, stat='probability')
+
+            return fig, ax
+
+
+    # Each MonteCarlo instance has a `pdf` property that is an instance of DistributionGenerator
+    # DistributionGenerator is an abstractive layer who main purpose is to map a user's input definitions to the appropriate numpy Generator
+    class DistributionGenerator:
+        def __init__(self, outer):
+            self.outer = outer
+
+
+        def _compile(self, distr_func, distr_type, **kwargs):
+            return self.outer.InputDict(outer=self.outer, obj={'distribution': distr_type, 'function': distr_func, 'args': {**kwargs}})
+
+        
+        def degenerate(self, val):
+            def degenerate_distribution(**kwargs):
+                if isinstance(val, int):
+                    return self.outer.rng.integers(low=val, high=val, endpoint=True)
+                else:
+                    return self.outer.rng.uniform(low=val, high=val)
+
+            return self._compile(degenerate_distribution, 'degenerate', val=val)
+
+
+        def uniform(self, low, high):
+            def uniform_distribution(**kwargs):
+                if all([isinstance(i, int) for i in (low, high)]):
+                    return self.outer.rng.integers(low, high, endpoint=True)
+                else:
+                    return self.outer.rng.uniform(low, high)
+            
+            return self._compile(uniform_distribution, 'uniform', low=low, high=high)
+
+
+        def normal(self, center, stddev):
+            def normal_distribution(**kwargs):
+                return self.outer.rng.normal(loc=center, scale=stddev)
+            
+            return self._compile(normal_distribution, 'normal', center=center, stddev=stddev)
+
+
+        def triangular(self, low, mode, high):
+            def trianguler_distribution(**kwargs):
+                return self.outer.rng.triangular(left=low, mode=mode, right=high)
+            
+            return self._compile(trianguler_distribution, 'triangular', low=low, mode=mode, high=high)
+
+
+        def PERT(self, low, mode, high, lamb=4):
+            def PERT_distribution(**kwargs):
+                r = high - low
+                alpha = 1 + lamb * (mode - low) / r
+                beta = 1 + lamb * (high - mode) / r        
+                return low + self.outer.rng.beta(alpha, beta) * r
+            
+            return self._compile(PERT_distribution, 'PERT', low=low, mode=mode, high=high, lamb=lamb)
 
     
-    # def simulation_confint(self, alpha):
-    #     pctiles = alpha/2, 1-alpha/2
-    #     ci_lower, ci_upper = np.quantile(self.simulations, np.array(pctiles))
-    #     return ci_lower, ci_upper
+    # A SimulationArray is an np.ndarray of simulation results with added attributes
+    class SimulationArray(np.ndarray):
+        def __new__(cls, a, samples=None):
+            obj = np.asarray(a).view(cls)
+            obj.samples = samples
+            return obj
 
 
-    def plot_simulation_distribution(self, fig=None, ax=None, confint=None):
-        if hasattr(self, 'simulations'):  
-            return self._plot_pdf(self.simulations, n=len(self.simulations), confint=confint, fig=fig, ax=ax)
+        def plot(self, kind='pdf', fig=None, ax=None):
+            cume = True if kind=='cdf' else False
+
+            if not fig and not ax:
+                fig, ax = plt.subplots()
+            
+            sns.histplot(self, ax=ax, kde=True, stat='probability', cumulative=cume)
+            
+            return fig, ax
